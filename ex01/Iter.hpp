13f7785d02e9cc645b5c3543576ba708e6c34bda
rgolfett <romane.golfetto@gmail.com>
2025-08-05 18:13:57 +0200

    ex02

--- ex01/Iter.hpp
+++ ex01/Iter.hpp
@@ -5,10 +5,16 @@
 #include <stdexcept>
 #include <typeinfo>
 
-template <typename T> void Iter(T x, T y, (void)(*funct2)(int a)) 
+template <typename T> void Iter(T *adress, size_t lengh, void funct(T &)) 
 {
- 
-    return (x > y) ? x : y;
+    size_t i = 0;
+    if (adress == NULL || funct == NULL)
+        return ;
+    while (i < lengh)
+    {
+        funct(adress[i]);
+        i++;
+    }
 }
 
 // Implement a function template iter that takes 3 parameters and returns nothing.
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,2 +1,15 @@
 #include "Iter.hpp"
 
+template<typename T>
+
+void nullifier(T &)
+{
+	std::cout << "NULL" << std::endl;
+	return;
+}
+
+int main(void)
+{
+	Iter("12345", 5, nullifier);
+	return (0);
+}
