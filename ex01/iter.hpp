978142215f807398256e9df26027ed17bb486dbf
Romane Golfetto <rgolfett@z3r5p1.42lyon.fr>
2025-08-29 22:10:22 +0200

    fixed const

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -5,7 +5,19 @@
 #include <stdexcept>
 #include <typeinfo>
 
-template <typename T> void Iter(T *adress, size_t lengh, void funct(const T &)) 
+template <typename T> void Iter(const T *adress, size_t lengh, void funct(const T &)) 
+{
+	size_t i = 0;
+	if (adress == NULL || funct == NULL)
+		return ;
+	while (i < lengh)
+	{
+		funct(adress[i]);
+		i++;
+	}
+}
+
+template <typename T> void Iter(T *adress, size_t lengh, void funct(T &)) 
 {
 	size_t i = 0;
 	if (adress == NULL || funct == NULL)
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,14 +2,24 @@
 
 template<typename T>
 
-void nullifier(T &)
+void nullifier(const T &)
 {
 	std::cout << "NULL" << std::endl;
 	return;
 }
 
+template<typename T>
+
+void print(T const & x )
+{ 
+	std::cout << x << std::endl; 
+	return;
+}
+
 int main(void)
 {
-	Iter("12345", 5, nullifier);
+	char str[] = "aaaaa";
+	Iter("12345", 5, print);
+	Iter(str, 6, nullifier);
 	return (0);
 }
